a8a6d50b6a29e34b07ad846153a479e13d3722f8
Callum Rigby <callum.rigby@udgroup.co.uk>
2017-07-21 09:21:03 +0100

    Add project files.

---
+++ DateImportUtility/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UDGroup.DataAccess;
+using UDGroup.General.Sql;
+
+namespace DateImportUtility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tableName = "matrix_tgp_postcodes";
+            string path = @"X:\UDMatrix Imports\_____Matrix Files - All Clients\Total Gas and Power\2017-07-20\GEM (UD Group)\postcode lookup.csv";
+            string whereClause = "";
+            SqlFunctions.BulkInsertToAzure(Constants.AzureBrokerDataConnectionString, "dbo", tableName, path, true, whereClause);
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
